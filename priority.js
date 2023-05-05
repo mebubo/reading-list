2585172575bd427f10964680a7c277c91b028934
Sergei Dolgov <dolgovs@gmail.com>
2023-05-05 19:39:36 +0200

    Read checkbox in priority

--- priority.js
+++ priority.js
@@ -9,23 +9,35 @@ function formatTime(timestamp) {
 }
 
 function calculatePriority({timestamps}) {
-    // Calculate a priority score based on recency and frequency
-    const recencyScore = (Date.now() - Math.max(...timestamps)) / (1000 * 60 * 60 * 24); // days ago
+    const recencyScore = (Date.now() - Math.max(...timestamps)) / (1000 * 60 * 60 * 24);
     return Math.pow(timestamps.length, 2) / recencyScore;
 }
 
-chrome.storage.local.get('readingList', data => {
-    const readingList = data.readingList || [];
+async function getReadingList() {
+    const data = await chrome.storage.local.get('readingList');
+    return data.readingList || [];
+}
+
+async function render() {
+    const readingList = await getReadingList();
     const listContainer = document.getElementById('list-container');
-    listContainer.classList.add('priority-view')
+    listContainer.innerHTML = '';
+    listContainer.classList.add('priority-view');
 
-    const pages = [...readingList]
+    const sortedPages = readingList.sort((a, b) => calculatePriority(b) - calculatePriority(a));
+    const unreadPages = sortedPages.filter(page => page.read === null);
 
-    const sortedReadingList = pages.sort((a, b) => calculatePriority(b) - calculatePriority(a));
-
-    for (const page of sortedReadingList) {
+    for (const page of unreadPages) {
         const listItem = document.createElement('div');
         listItem.classList.add('list-item');
+
+        const readCheckbox = document.createElement('input');
+        readCheckbox.type = 'checkbox';
+        readCheckbox.addEventListener('change', async () => {
+            const updatedReadingList = readingList.map(item => item.url === page.url ? {...item, read: readCheckbox.checked ? new Date().getTime() : null} : item);
+            await chrome.storage.local.set({readingList: updatedReadingList});
+            render();
+        });
 
         const timestampContainer = document.createElement('div');
         for (const time of page.timestamps.reverse()) {
@@ -43,22 +55,21 @@ chrome.storage.local.get('readingList', data => {
         link.textContent = page.title;
 
         const deleteButton = document.createElement('button');
-        deleteButton.classList.add('delete-button');
         deleteButton.textContent = '\u2716';
-
-        deleteButton.addEventListener('click', () => {
-          const filtered = readingList.filter(item => !(item.url === page.url));
-          // const index = readingList.findIndex(item => item.url === page.url && item.time == page.time);
-          // readingList.splice(index, 1);
-          chrome.storage.local.set({ readingList: filtered });
-          listItem.remove();
+        deleteButton.classList.add('delete-button');
+        deleteButton.addEventListener('click', async () => {
+            const updatedReadingList = readingList.filter(item => item.url !== page.url);
+            await chrome.storage.local.set({readingList: updatedReadingList});
+            render();
         });
 
-
         listItem.appendChild(deleteButton);
+        listItem.appendChild(readCheckbox);
         listItem.appendChild(timestampContainer);
         listItem.appendChild(favicon);
         listItem.appendChild(link);
         listContainer.appendChild(listItem);
     }
-});
+}
+
+render();
