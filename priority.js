1de89781748243e315a954b31ebebca9207c59b6
Sergei Dolgov <dolgovs@gmail.com>
2023-05-05 20:46:32 +0200

    Snapshot

--- priority.js
+++ priority.js
@@ -7,7 +7,16 @@ async function renderPriority() {
     const readingList = await getReadingList();
     const unreadPages = readingList.filter(page => page.read === null);
 
-    render(unreadPages, async (page) => {
+    // Sort the unreadPages list based on the priority calculated by the calculatePriority function
+    const sortedUnreadPages = unreadPages.sort((a, b) => {
+        const priorityA = calculatePriority(a);
+        const priorityB = calculatePriority(b);
+
+        // Sort in descending order, so higher priority items come first
+        return priorityB - priorityA;
+    });
+
+    render(sortedUnreadPages, async (page) => {
         const updatedReadingList = readingList.filter(item => item.url !== page.url);
         await chrome.storage.local.set({readingList: updatedReadingList});
         renderPriority();
