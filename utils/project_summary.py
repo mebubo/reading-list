cb668496b64869e0f8e1d144515f0959755d9954
Sergei Dolgov <dolgovs@gmail.com>
2023-06-18 20:12:27 +0200

    utils/project_summary.py: exclude .html

--- utils/project_summary.py
+++ utils/project_summary.py
@@ -3,7 +3,7 @@ import os
 def dir_summary(directory_path):
     filenames = [filename for filename in os.listdir(directory_path) if os.path.isfile(filename)]
     prompt_first = [file_contents("PROMPT.md")] + [file_summary(f) for f in filenames
-        if f != "PROMPT.md" and f != "shell.nix" and not f.endswith(".css")]
+        if f != "PROMPT.md" and f != "shell.nix" and not f.endswith(".css") and not f.endswith(".html")]
     return "\n".join(prompt_first)
 
 def file_contents(filename):
